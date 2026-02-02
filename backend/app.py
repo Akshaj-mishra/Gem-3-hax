f8f66f34e5bea3cc419a2dcb08e320c134eefdf7
Akshaj Mishra <141712076+Akshaj-mishra@users.noreply.github.com>
2026-02-02 16:00:48 +0530

    added backend

---
+++ backend/app.py
@@ -0,0 +1,55 @@
+from fastapi import FastAPI, UploadFile, File, Form, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+import numpy as np
+import google.generativeai as genai
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+api_key = os.getenv("GOOGLE_API_KEY") 
+
+
+try:
+    genai.configure(api_key=api_key)
+    gemini_model = genai.GenerativeModel("gemini-3-flash-preview")
+    print("✅ Gemini AI configured successfully")
+    
+    
+except Exception as e:
+    print(f"❌ Error configuring Gemini AI: {e}")
+    gemini_model = None
+
+
+@app.get("/")
+def root():
+    return {"message": "🚀 FastAPI backend is running and CORS is enabled!"}
+
+
+@app.post("/chat")
+async def analyze_plant(question: str = Form("")):
+
+    try:
+        response = gemini_model.generate_content(question)
+
+        return {
+            "solution": response.text,
+        }
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Processing error: {str(e)}")
+
